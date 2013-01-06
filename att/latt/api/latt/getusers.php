235586860b43c58d9d7a46277010dd5743aca01b
Adrian Kimmitt <adrian@appslv.com>
2013-01-06 05:20:30 -0800

    ATT_APIPlatform_SampleApps / RESTFul / CMS / PHP / app1 /

---
+++ att/latt/api/latt/getusers.php
@@ -0,0 +1,9 @@
+<?php
+
+include "../config.inc.php";
+
+header('Content-type: application/json');
+
+echo getusers();
+
+?>
---
+++ att/latt/cms/config.php
@@ -0,0 +1,9 @@
+<?php
+    $api_key = "e8ba381eb17df3bb982c7641f59287c4";
+    $secret_key = "6c20b68ca97df289";
+    $FQDN = "https://api.att.com";
+    $oauth_file = "ccsoauthtoken.php";
+    $scope = "CCS";
+    $callcontrol_file = "callcontrolscript.php";
+    $number = "17027699388";
+?>
