e8c1263b83d6cb90dbad8082c78dbfd94fb3ab9d
William Ryan Roper <wrroper+git@gmail.com>
2013-01-05 22:36:05 -0700

    Change Server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var sql = require('node-sqlserver');
+var sql = require('msnodesql');
 var conn_str = "Driver={SQL Server Native Client 10.0};Server=tcp:mj7i58or45.database.windows.net,1433;Database=gmfleet;Uid=gmfleet@mj7i58or45;Pwd=Fleet1234;Encrypt=yes;Connection Timeout=30;";
 
 var http = require('http');
