5452dd6d619bd662265acb7386545b2273895d5a
William Ryan Roper <wrroper+git@gmail.com>
2013-01-06 04:50:18 -0700

    changes

--- server.js
+++ server.js
@@ -65,9 +65,18 @@ app.post('/posttrip', function(req, res) {
             }
         });
 
-        sql.query(conn_str, insert, [carid, item.trip_date, item.odometer, item.engine_oil_life, item.tire_left_front_pressure, item.tire_right_front_pressure,
-                                     item.tire_left_rear_pressure, item.tire_right_rear_pressure, item.fuel_level, item.gps_lat, item.gps_long, item.tire_condition,
-                                     item.tire_comment, item.glass_condition, item.glass_comment, item.body_condition, item.body_comment, item.trip_comment, item.user_id], function(err) {
+        qry = "INSERT INTO JSON (JSON) VALUES (?)";
+        var params = [carid, item.trip_date, item.odometer, item.engine_oil_life, item.tire_left_front_pressure, item.tire_right_front_pressure,
+            item.tire_left_rear_pressure, item.tire_right_rear_pressure, item.fuel_level, item.gps_lat, item.gps_long, item.tire_condition,
+            item.tire_comment, item.glass_condition, item.glass_comment, item.body_condition, item.body_comment, item.trip_comment, item.user_id];
+
+        sql.query(conn_str, qry, [JSON.stringify(params)], function(err, results) {
+            res.send("Got Error 1 " + err);
+            res.end("");
+            return;
+        }) ;
+
+        sql.query(conn_str, insert, , function(err) {
             if(err) {
                 res.send("Got Error 3 " + err);
                 res.end("");
